8af4763b1da44c5a6228d229c6548b2a9a685135
Brent Reininger <brent.reininger89@gmail.com>
2019-06-13 19:56:24 -0700

    Add Nav Items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavitaionItem";
+
+const navigationItems = props => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      Burger
+    </NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
